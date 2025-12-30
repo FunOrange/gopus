de940a774685476d41b65390fc567cd4511d3b8f
FunOrange <junarvi@yahoo.ca>
2025-12-30 15:33:48 -0500

    add opus_encoder_create, opus_encoder_destroy

--- gopus.go
+++ gopus.go
@@ -2,8 +2,60 @@ package gopus
 
 import (
 	"fmt"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-func Hello() {
-	fmt.Println("Hello World!")
+var (
+	opus                 = windows.NewLazyDLL("opus.dll")
+	opus_encoder_create  = opus.NewProc("opus_encoder_create")
+	opus_encoder_destroy = opus.NewProc("opus_encoder_destroy")
+)
+
+type Encoder uintptr // opaque pointer to an Opus encoder
+
+type Application int
+
+const (
+	// Opus Application Constants
+	OPUS_APPLICATION_VOIP                Application = 2048
+	OPUS_APPLICATION_AUDIO               Application = 2049
+	OPUS_APPLICATION_RESTRICTED_LOWDELAY Application = 2051
+
+	// Opus Error Codes
+	OPUS_OK               = 0
+	OPUS_BAD_ARG          = -1
+	OPUS_BUFFER_TOO_SMALL = -2
+	OPUS_INTERNAL_ERROR   = -3
+	OPUS_INVALID_PACKET   = -4
+	OPUS_UNIMPLEMENTED    = -5
+	OPUS_INVALID_STATE    = -6
+	OPUS_ALLOC_FAIL       = -7
+)
+
+type Error int32
+
+func (e Error) Error() string {
+	return fmt.Sprintf("opus: error code %d", int32(e))
+}
+
+func CreateEncoder(fs int, channels int, application Application) (Encoder, error) {
+	var errCode int32
+	encoder, _, _ := opus_encoder_create.Call(
+		uintptr(int32(fs)),
+		uintptr(int32(channels)),
+		uintptr(int32(application)),
+		uintptr(unsafe.Pointer(&errCode)),
+	)
+	if errCode != OPUS_OK {
+		return 0, Error(errCode)
+	}
+	return Encoder(encoder), nil
+}
+
+func (e Encoder) Destroy() {
+	if e != 0 {
+		opus_encoder_destroy.Call(uintptr(e))
+	}
 }
---
+++ gopus_test.go
@@ -0,0 +1,39 @@
+package gopus
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestCreateAndDestroyEncoder(t *testing.T) {
+	// create encoder
+	fs := 48000
+	channels := 2
+	application := OPUS_APPLICATION_AUDIO
+	encoder, err := CreateEncoder(fs, channels, application)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// destroy encoder
+	encoder.Destroy()
+}
+
+func TestCreateEncoderWithBadArguments(t *testing.T) {
+	fs := 48000
+	channels := -1
+	application := OPUS_APPLICATION_AUDIO
+	_, err := CreateEncoder(fs, channels, application)
+	if err != nil {
+		var oErr Error
+		if errors.As(err, &oErr) {
+			switch oErr {
+			case OPUS_BAD_ARG:
+				return
+			default:
+				t.Fatal("expected bad argument error")
+			}
+		}
+	}
+	t.Fatal("expected bad argument error")
+}
